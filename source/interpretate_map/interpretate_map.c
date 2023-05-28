9180fafddf3b513fe7ba08a9a36dff499a16efe4
Gustavo Sousa <gfsousa42@gmail.com>
2023-05-28 20:45:20 +0000

    Refactor: dividing interpattion in files

--- source/interpretate_map/interpretate_map.c
+++ source/interpretate_map/interpretate_map.c
@@ -15,31 +15,6 @@ void    interpretate_line(t_game *game, char *line)
     }
 }
 
-int has_all_information(t_game *game)
-{
-    if (game->map.mtx == NULL)
-        return (0);
-    game->map.mtx[game->map.height] = NULL;
-    if (!game->texture.north.ptr
-        || !game->texture.south.ptr
-        || !game->texture.west.ptr
-        || !game->texture.east.ptr
-        || game->texture.ceiling == -1
-        || game->texture.floor == -1)
-        return (0);
-    return (1);
-}
-
-int open_file(char *file_path)
-{
-    int fd;
-
-    fd = open(file_path, O_RDONLY);
-    if (fd == -1)
-        exit_game("File non-existed or non-permission granted", NULL);
-    return (fd);
-}
-
 // Leio a linha
 // E guardo ou na textura ou na matrix.
 void    interpretate_map(t_game *game, char *file_path)
