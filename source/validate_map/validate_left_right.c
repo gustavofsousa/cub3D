e4f8aacc47c774fa66e4bf92723860773b93f668
Gustavo Sousa <gusousa@student.42.rio>
2023-05-31 13:26:57 -0300

    feat: is valid at pos ()ft

--- source/validate_map/validate_left_right.c
+++ source/validate_map/validate_left_right.c
@@ -21,6 +21,13 @@ int find_init_line(char *line)
     return (i);
 }
 
+int is_valid_at_pos(char *s1, char *s2, int init_s1, int init_s2)
+{
+    if (s1[init_s1] == '0' || s2[init_s2] == '0')
+        return (0);
+    return (1);
+}
+
 int validate_left(t_game *game)
 {
     int     i;
@@ -36,6 +43,8 @@ int validate_left(t_game *game)
         s2 = game->map.mtx[i];
         init_s1 = find_init_line(s1);
         init_s2 = find_init_line(s2);
+        if (!is_valid_at_pos(s1, s2, init_s1, init_s2))
+            return (0);
         if (init_s1 < init_s2 && !is_valid_wall(s1, init_s1, init_s2))
             return (0);
         else if (init_s1 > init_s2 && !is_valid_wall(s2, init_s2, init_s1))
@@ -59,6 +68,8 @@ int    validate_right(t_game *game)
         s2 = game->map.mtx[i];
         len_s1 = ft_strlen(s1);
         len_s2 = ft_strlen(s2);
+        if (!is_valid_at_pos(s1, s2, len_s1, len_s2))
+            return (0);
         if (len_s1 < len_s2 && !is_valid_wall(s2, len_s1, len_s2))
             return (0);
         else if (len_s1 > len_s2 && !is_valid_wall(s1, len_s2, len_s1))
