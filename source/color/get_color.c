b21f621f8f204de04c65dbb27027b48a22df43be
Gustavo Sousa <gusousa@student.42.rio>
2023-06-01 11:55:06 -0300

    feat: receiving rgb color

--- source/color/get_color.c
+++ source/color/get_color.c
@@ -1,10 +1,42 @@
 #include "../../include/cub3d.h"
 
+int is_valid_number(char **mtx, int r, int g, int b)
+{
+    if (mtx[0][0]  == '-'
+        || mtx[1][0] == '-'
+        || mtx [2][0] ==  '-')
+        return (0);
+    if (ft_strncmp(mtx[0], ft_itoa(r), ft_strlen(mtx[0])))
+        return (0);
+    if (ft_strncmp(mtx[1], ft_itoa(g), ft_strlen(mtx[1])))
+        return (0);
+    if (ft_strncmp(mtx[2], ft_itoa(b), ft_strlen(mtx[2])))
+        return (0);
+    return (1);
+}
+
 int translate_rgb(char *rgb, t_game *game)
 {
-    (void)game;
-    (void)rgb;
-    return (1);
+    int r;
+    int g;
+    int b;
+    int c;
+    char**lonely;
+
+    lonely = ft_split(rgb, ',');
+    r = ft_atoi(lonely[0]);
+    g = ft_atoi(lonely[1]);
+    b = ft_atoi(lonely[2]);
+    if (!is_valid_number(lonely, r, g, b))
+    {
+        free_matrix(lonely);
+        exit_game("Number rgb not valid", game);
+    }
+    c = r;
+    c = (c << 8) | g;
+    c = (c << 8) | b;
+    free_matrix(lonely);
+    return (c);
 }
 
 void get_color(t_game *game, char *line)
