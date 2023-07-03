ad29def87f0e69ba1565ff070ecdf33c39b63b91
Gustavo Sousa <gfsousa42@gmail.com>
2023-07-03 16:08:21 -0300

    refactor: create rotate_vectors ft

--- source/movements/turn.c
+++ source/movements/turn.c
@@ -6,40 +6,37 @@
 /*   By: gusta <gusta@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 09:42:07 by fcaetano          #+#    #+#             */
-/*   Updated: 2023/07/03 15:30:59 by gusta            ###   ########.fr       */
+/*   Updated: 2023/07/03 16:07:27 by gusta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-void	turn_left(t_player *player)
+void	rotate_vectors(double *x, double *y, double angle)
 {
 	double	old_x;
 
-	old_x = (player->dir_x);
-	player->dir_x = player->dir_x * cos(-player->rot_speed)
-		- player->dir_y * sin(-player->rot_speed);
-	player->dir_y = old_x * sin(-player->rot_speed)
-		+ player->dir_y * cos(-player->rot_speed);
-	old_x = player->cam_plane_dir_x;
-	player->cam_plane_dir_x = player->cam_plane_dir_x * cos(-player->rot_speed)
-		- player->cam_plane_dir_y * sin(-player->rot_speed);
-	player->cam_plane_dir_y = old_x * sin(-player->rot_speed)
-		+ player->cam_plane_dir_y * cos(-player->rot_speed);
+	old_x = *x;
+	*x = (*x) * cos(angle) - (*y) * sin(angle);
+	*y = old_x * sin(angle) + (*y) * cos(angle);
+}
+
+void	turn_left(t_player *player)
+{
+	rotate_vectors(&(player->dir_x),
+		&(player->dir_y),
+		-player->rot_speed);
+	rotate_vectors(&(player->cam_plane_dir_x),
+		&(player->cam_plane_dir_y),
+		-player->rot_speed);
 }
 
 void	turn_right(t_player *player)
 {
-	double	old_x;
-
-	old_x = (player->dir_x);
-	player->dir_x = player->dir_x * cos(player->rot_speed)
-		- player->dir_y * sin(player->rot_speed);
-	player->dir_y = old_x * sin(player->rot_speed)
-		+ player->dir_y * cos(player->rot_speed);
-	old_x = player->cam_plane_dir_x;
-	player->cam_plane_dir_x = player->cam_plane_dir_x * cos(player->rot_speed)
-		- player->cam_plane_dir_y * sin(player->rot_speed);
-	player->cam_plane_dir_y = old_x * sin(player->rot_speed)
-		+ player->cam_plane_dir_y * cos(player->rot_speed);
+	rotate_vectors(&(player->dir_x),
+		&(player->dir_y),
+		player->rot_speed);
+	rotate_vectors(&(player->cam_plane_dir_x),
+		&(player->cam_plane_dir_y),
+		player->rot_speed);
 }
