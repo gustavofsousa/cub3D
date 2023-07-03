3a2a7f1cf713021975062f1f0a514129bd320f0f
Gustavo Sousa <gfsousa42@gmail.com>
2023-07-03 16:41:05 -0300

    fix: player entering in the wall at side

--- source/movements/walk.c
+++ source/movements/walk.c
@@ -6,7 +6,7 @@
 /*   By: gusta <gusta@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 14:45:32 by gusousa           #+#    #+#             */
-/*   Updated: 2023/07/03 16:09:13 by gusta            ###   ########.fr       */
+/*   Updated: 2023/07/03 16:39:39 by gusta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ void	walk_right(t_player *player, t_map *map)
 	min_dist = 0.2;
 	new_x = player->x + player->cam_plane_dir_x * player->speed;
 	new_y = player->y + player->cam_plane_dir_y * player->speed;
-	if (player->cam_plane_dir_y < 0)
+	if (player->dir_y > 0)
 		min_dist *= -1;
 	if (map->mtx_int[(int)(new_x + min_dist)]
 					[(int)(player->y)] == 0)
 		player->x = new_x;
 	min_dist = 0.2;
-	if (player->cam_plane_dir_x < 0)
+	if (player->dir_x < 0)
 		min_dist *= -1;
 	if (map->mtx_int[(int)(player->x)]
 					[(int)(new_y + min_dist)] == 0)
@@ -43,13 +43,13 @@ void	walk_left(t_player *player, t_map *map)
 	min_dist = 0.2;
 	new_x = player->x - player->cam_plane_dir_x * player->speed;
 	new_y = player->y - player->cam_plane_dir_y * player->speed;
-	if (player->cam_plane_dir_x < 0)
+	if (player->dir_y > 0)
 		min_dist *= -1;
 	if (map->mtx_int[(int)(new_x - min_dist)]
 					[(int)(player->y)] == 0)
 		player->x = new_x;
 	min_dist = 0.2;
-	if (player->cam_plane_dir_y < 0)
+	if (player->dir_x < 0)
 		min_dist *= -1;
 	if (map->mtx_int[(int)(player->x)]
 					[(int)(new_y - min_dist)] == 0)
