595ae8194beb5ac3e1bbc7ba81b98fe1305ba28c
Gustavo Sousa <gfsousa42@gmail.com>
2023-07-03 11:49:19 -0300

    playting the game refactor

--- source/render_game.c
+++ source/render_game.c
@@ -6,36 +6,43 @@
 /*   By: gusta <gusta@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 09:37:54 by fcaetano          #+#    #+#             */
-/*   Updated: 2023/07/03 11:38:59 by gusta            ###   ########.fr       */
+/*   Updated: 2023/07/03 11:48:57 by gusta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int	loop_hook(t_game *game)
+void	moviment_player(t_game *game)
 {
-	if (game->key.w_is_press + game->key.s_is_press + game->key.d_is_press
-		+ game->key.ra_is_press + game->key.la_is_press + game->key.a_is_press)
-	{
-		if (game->key.w_is_press)
-			walk_forward (game);
-		if (game->key.a_is_press)
-			walk_left (game);
-		if (game->key.s_is_press)
-			walk_backward (game);
-		if (game->key.d_is_press)
-			walk_right (game);
-		if (game->key.ra_is_press)
-			turn_right (&game->player);
-		if (game->key.la_is_press)
-			turn_left (&game->player);
-	}
-	render_map3d(game);
+	if (game->key.w_is_press)
+		walk_forward(game);
+	if (game->key.a_is_press)
+		walk_left(game);
+	if (game->key.s_is_press)
+		walk_backward(game);
+	if (game->key.d_is_press)
+		walk_right(game);
+	if (game->key.ra_is_press)
+		turn_right(&game->player);
+	if (game->key.la_is_press)
+		turn_left(&game->player);
+}
+
+
+void	render_minimap(t_game *game)
+{
 	if (game->key.m_is_press)
 	{
     	render_map2d(game, SIZE_MMAP);
     	render_player(&game->img, game->player, 9);
 	}
+}
+
+int	playing_the_game(t_game *game)
+{
+	moviment_player(game);
+	render_map3d(game);
+	render_minimap(game);
 	mlx_put_image_to_window(game->img.ptr, game->img.win,
 		game->img.img, 0, 0);
 	//mlx_destroy_window (game->img.ptr, game->img.win);
@@ -47,8 +54,6 @@ void	render_game(t_game *game)
 	mlx_clear_window(game->img.ptr, game->img.win);
 	render_map3d(game);
 	prepare_hooks_keys(game);
-	mlx_loop_hook(game->img.ptr, loop_hook, game);
-	mlx_put_image_to_window(game->img.ptr, game->img.win,
-		game->img.img, 0, 0);
+	mlx_loop_hook(game->img.ptr, playing_the_game, game);
 	mlx_loop(game->img.ptr);
 }
